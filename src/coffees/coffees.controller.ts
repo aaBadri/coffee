28791423d7fc12ca212b33ec073641aadfaf4fe1
AliAkbarBadri <aabadri74@gmail.com>
2023-03-23 12:28:40 +0330

    add parameter to url

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('flavors')
   findAll() {
     return 'return coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param() param) {
+    return `id: ${param.id}`;
   }
 }
