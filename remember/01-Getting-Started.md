792d9b1511137f711ec706919e97f13e39075591
AliAkbarBadri <aliakbarbadr@gmail.com>
2023-10-10 11:46:27 +0330

    restart everything

---
+++ remember/01-Getting-Started.md
@@ -0,0 +1,9 @@
+1. install node and npm:
+   1. curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+   2. nvm list-remote
+   3. nvm install v18.18.0
+2. install nestjs
+   1. npm i -g @nestjs/cli
+3. create project
+   1. nest new
+   2. 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [
-    CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
-      autoLoadEntities: true,
-      // turn off in production
-      synchronize: true,
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
