c1355ba0408d0dc64b0d5502700bb2cd30b668a0
AliAkbarBadri <aliakbarbadr@gmail.com>
2023-10-20 12:58:08 +0330

    add basic swagger ui to app

---
+++ remember/07 Generating OpenAPI Specification.md
@@ -0,0 +1,6 @@
+# 61
+npm i @nestjs/swagger swagger-ui-express
+
+Let's open up our main.ts file and start setting up swagger by generating a base "document" which will be the base object where our OpenAPI document is contained.
+
+DocumentBuilder from the @nestjs/swagger package, helps us structure a base document that conforms to the OpenAPI specification.
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,14 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
+  const options = new DocumentBuilder()
+    .setTitle('Coffees app')
+    .setDescription('a description for coffees app')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  // api_doc is the route path for swagger ui
+  SwaggerModule.setup('api_doc', app, document);
   await app.listen(3000);
 }
 bootstrap();
