7aa9405cfb70493ab15c73db60ed360c18655b61
AliAkbarBadri <aliakbarbadr@gmail.com>
2023-10-26 19:17:17 +0330

    implement e2e test logic

--- test/coffee/coffees.e2e-spec.ts
+++ test/coffee/coffees.e2e-spec.ts
@@ -1,13 +1,20 @@
+import 'jest';
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-// import * as request from 'supertest';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
 import { CoffeesModule } from 'src/coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
+import { CreateCoffeeDto } from 'src/coffees/dto/create-coffee.dto';
 
 dotenv.config();
 
 describe('[Feature] Coffees - /coffees', () => {
+  const coffee = {
+    name: 'Coffee1',
+    brand: 'Brand1',
+    flavors: ['chocolate', 'vanilla'],
+  };
   let app: INestApplication;
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -27,9 +34,57 @@ describe('[Feature] Coffees - /coffees', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+        transformOptions: { enableImplicitConversion: true },
+      }),
+    );
     await app.init();
   });
-  it.todo('Create [POST /]');
+  // can use todo to track tests that need to be implemented
+  it('Create [POST /]', () => {
+    return request(app.getHttpServer())
+      .post('/coffees')
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.CREATED)
+      .then(({ body }) => {
+        expect(body).toEqual(
+          expect.objectContaining({
+            ...coffee,
+            flavors: expect.arrayContaining(
+              coffee.flavors.map((name) => expect.objectContaining({ name })),
+            ),
+          }),
+        );
+      });
+  });
+
+  //   it('Get all [GET /]', () => {
+  //     return request(app.getHttpServer())
+  //       .get('/coffees')
+  //       .send(coffee as CreateCoffeeDto)
+  //       .expect(HttpStatus.OK);
+  //   });
+
+  // it('Get one [GET /:id]', () => {
+  //   return request(app.getHttpServer())
+  //     .get('/coffees/1')
+  //     .expect(HttpStatus.OK)
+  //     .then(({ body }) => {
+  //       const expectedCoffee = jasmine.objectContaining({
+  //         ...coffee,
+  //         flavors: jasmine.arrayContaining(
+  //           coffee.flavors.map(name => jasmine.objectContaining({ name })),
+  //         ),
+  //       });
+  //
+  //       expect(body).toEqual(expectedCoffee);
+  //     });
+  // });
+
   it.todo('Get all [GET /]');
   it.todo('Get one [GET /:id]');
   it.todo('Update one [PATCH /:id]');
